7d0edaeaa87d3a175b3dc2f72952274497eb6642
Alex Dymov <a8dymov@gmail.com>
2020-02-09 11:50:05 +0500

    working now: opening watchlist to a full screen, local storage

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ import { TMDB_KEY } from "./config.js"; // API key for movie database
 const endpointSearch = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=`;
 
 const sideBar = document.querySelector('.side-bar-wrapper');
+//const sideBarButton = document.querySelector('.side-bar-wrapper:before');
+const sideBarOpenButton = document.querySelector('.open-button');
 const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 const sideBarContent = document.querySelector('.cards');
 const sideBarCloseButton = document.querySelector('.close-button');
-console.log(sideBarCloseButton);
 
 
-let moviesInWatchList = /*JSON.parse(localStorage.getItem('moviesInWatchList') || */[]; //creating empty array or getting watch list from local storage
+let moviesInWatchList = JSON.parse(localStorage.getItem('moviesInWatchList')) || []; //creating empty array or getting watch list from local storage
 
 function showWatchList() {
     const sideBarHTML = moviesInWatchList.map(movie => {
-        console.log(movie);
         return `
             <div class="card" data-description="${movie.id}">
                 <div class="poster">
-                    <img src="${movie.img}">
-                    <span class="score score-${scoreColor(movie.score)}">${movie.score}</span>
+                    <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}">
+                    <span class="score score-${scoreColor(movie.vote_average)}">${movie.vote_average}</span>
                     <span class="remove" name="remove"><i class="fas fa-trash"></i></span>
                 </div>
                 <h2>${movie.title}</h2>
@@ -30,116 +30,87 @@ function showWatchList() {
         }).join('');
     sideBarContent.innerHTML = sideBarHTML;
     const cards = document.querySelectorAll('.card');
-    cards.forEach(card => card.addEventListener('click', openModalX));
+    cards.forEach(card => card.addEventListener('click', (e) => openModal(e)(moviesInWatchList)));
 }
 
 function addToWatchList(e){
-    
-    //if(e.target.name === 'add'){
     if(e.target.name === 'btn'){
         if(!e.target.classList.contains('remove')){
             console.log(e.target);
-
             const movieData = {
                 id: e.target.dataset.id,
                 title: e.target.dataset.title,
-                release: e.target.dataset.release,
-                img: e.target.dataset.img,
-                score: e.target.dataset.score,
+                release_date: e.target.dataset.release_date,
+                poster_path: e.target.dataset.poster_path,
+                vote_average: e.target.dataset.vote_average,
                 overview: e.target.dataset.overview,
             };
-            console.log(movieData);
             moviesInWatchList.push(movieData);
             e.target.innerText = 'Remove';
             e.target.classList.add('remove');
-            //e.target.name === 'remove';
-            //console.log(modalInner.innerHTML);
             showWatchList();
-            //localStorage.setItem('moviesInWatchList', JSON.stringify(moviesInWatchList));
+            localStorage.setItem('moviesInWatchList', JSON.stringify(moviesInWatchList));
         } else {
-        //if(e.target.name === 'remove'){
-
             moviesInWatchList = moviesInWatchList.filter(movie => movie.id !== e.target.dataset.id);
             e.target.innerText = 'Add';
             e.target.classList.remove('remove');
-            //e.target.name === 'add';
-
-            //console.log(modalInner.innerHTML);
             showWatchList();
-            //localStorage.setItem('moviesInWatchList', JSON.stringify(moviesInWatchList));
+            localStorage.setItem('moviesInWatchList', JSON.stringify(moviesInWatchList));
         }
     }    
 }
 
-//let isSideBarOpened = false;
-function openSideBar(){
-    //if(!isSideBarOpened){
+function openSideBar(e){
+    if(sideBar.classList.contains('open')){
+        sideBar.classList.add('open-full');
+    } else {
         sideBar.classList.add('open');
-        //isSideBarOpened = true;
-    //} else {
-        //sideBar.classList.remove('open');
-        //sideBar.classList.add('open-full');
-        //isSideBarOpened = false;
-    //}
+        sideBarOpenButton.firstElementChild.innerText = 'to full screen'.toUpperCase();
+    }
 }
 
 function closeSideBar(){
-    sideBar.classList.remove('open', 'open-full');
+    sideBar.classList.remove('open');
+    sideBar.classList.remove('open-full');
+    sideBarOpenButton.firstElementChild.innerText = 'Watchlist';
 }
 
-function openModalX(e){
-    if(e.target.closest(`[name='remove']`)){
-        console.log(moviesInWatchList);
-        moviesInWatchList = moviesInWatchList.filter(movie => movie.id !== e.currentTarget.dataset.description);
-        showWatchList();
-        return;
-    }
-    const movieCard = searchMovies.filter(movie => movie.id.toString() === e.currentTarget.dataset.description);
-    modalInner.innerHTML = `
-            <button class="add-remove-btn ${moviesInWatchList.some(movie => movie.id === movieCard[0].id.toString()) ? 'remove' : ''}"
-                name="btn"
-                data-id="${movieCard[0].id}"
-                data-title="${movieCard[0].title}"
-                data-img="https://image.tmdb.org/t/p/original/${movieCard[0].poster_path}"
-                data-score="${movieCard[0].vote_average}"
-                data-overview="${movieCard[0].overview}">${moviesInWatchList.some(movie => movie.id === movieCard[0].id.toString()) ? 'Remove' : 'Add'}</button>
-            <div class="movie-info">
-                <div class="movie-img">
-                    <img src="https://image.tmdb.org/t/p/original/${movieCard[0].poster_path}">
-                    <span class="score-${scoreColor(movieCard[0].vote_average)}">${movieCard[0].vote_average}</span>
-                </div>
-				<div class="movie-text">
-                    <h2>${movieCard[0].title}</h2>
-                    <p class="year">${movieCard[0].release_date.slice(0, 4)}</p>
-                    <p>${movieCard[0].overview}</p>
-				</div>
-			</div>
-    `;
-    modalOuter.classList.add('open');
-}
-
-
-/*
 function openModal(e){
-    const currentCard = e.currentTarget;
-    console.log(currentCard);
-    const movieCard = moviesInWatchList.filter(movie => movie.id.toString() === currentCard.dataset.description);
-
-    modalInner.innerHTML = `
-			<span class="arrow">←</span>description
-				<img src="https://image.tmdb.org/t/p/original/${movieCard[0].poster_path}">
-				<div class="movie-text">
-					<h2>${movieCard[0].title}</h2>
-					<p>${movieCard[0].overview}</p>
-				</div>
-				<span>${movieCard[0].vote_average}</span>
-			</div>
-            <span class="arrow">→</span>
-    `;
-	
-	modalOuter.classList.add('open');
+    return function (movies = searchMovies){
+        console.log(this);
+        console.log(e.target);
+        console.log(movies);
+        if(e.target.closest(`[name='remove']`)){
+            console.log(moviesInWatchList);
+            moviesInWatchList = moviesInWatchList.filter(movie => movie.id !== e.currentTarget.dataset.description);
+            showWatchList();
+            return;
+        }
+        const movieCard = movies.filter(movie => movie.id.toString() === e.currentTarget.dataset.description);
+        modalInner.innerHTML = `
+                <button class="add-remove-btn ${moviesInWatchList.some(movie => movie.id === movieCard[0].id.toString()) ? 'remove' : ''}"
+                    name="btn"
+                    data-id="${movieCard[0].id}"
+                    data-title="${movieCard[0].title}"
+                    data-poster_path="${movieCard[0].poster_path}"
+                    data-release_date="${movieCard[0].release_date}"
+                    data-vote_average="${movieCard[0].vote_average}"
+                    data-overview="${movieCard[0].overview}">${moviesInWatchList.some(movie => movie.id === movieCard[0].id.toString()) ? 'Remove' : 'Add'}</button>
+                <div class="movie-info">
+                    <div class="movie-img">
+                        <img src="https://image.tmdb.org/t/p/original/${movieCard[0].poster_path}">
+                        <span class="score-${scoreColor(movieCard[0].vote_average)}">${movieCard[0].vote_average}</span>
+                    </div>
+                    <div class="movie-text">
+                        <h2>${movieCard[0].title}</h2>
+                        <p class="year">${movieCard[0].release_date.slice(0, 4)}</p>
+                        <p>${movieCard[0].overview}</p>
+                    </div>
+                </div>
+        `;
+        modalOuter.classList.add('open');
+    }
 }
-*/
 
 function closeModal(){
 	modalOuter.classList.remove('open');
@@ -156,6 +127,7 @@ function scoreColor(score){
 }
 
 let searchMovies = [];
+
 function displayMatches(e){
     if(e.target.value === ''){
         searchMovies = [];
@@ -175,7 +147,7 @@ function displayMatches(e){
             }).join('');
             suggestions.innerHTML = searchHTML;
             const lists = document.querySelectorAll('.li');
-            lists.forEach(list => list.addEventListener('click', openModalX));
+            lists.forEach(list => list.addEventListener('click', (e) => openModal(e)(searchMovies)));
         });
 }
 
@@ -192,12 +164,13 @@ modalOuter.addEventListener('click', e => {
 	}
 });
 
-sideBar.addEventListener('click', openSideBar);
+sideBarOpenButton.addEventListener('click', openSideBar);
 sideBarCloseButton.addEventListener('click', closeSideBar);
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+modalInner.addEventListener('click', addToWatchList);
 
-
+showWatchList();
 
 /*
 const endpoint = `https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB_KEY}&language=en-US&page=1`;
@@ -221,6 +194,3 @@ function showTopRatedFilms() {
     cards.forEach(card => card.addEventListener('click', openModal));
 }
 */
-
-modalInner.addEventListener('click', addToWatchList);
-showWatchList();
